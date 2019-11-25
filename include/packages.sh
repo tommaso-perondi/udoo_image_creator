c2d0f20cda4f8933adc5ef4974b5fe9848150be2
ektor5 <ek5.chimenti@gmail.com>
2019-11-25 18:43:25 +0100

    refactoring installing packages

--- include/packages.sh
+++ include/packages.sh
@@ -8,14 +8,14 @@ LOG_FILE_PATH="log/out.log"
 DIR_PACKAGES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR_PACKAGES/utils/color.sh"
 ################################################################################
-
-ROOTFS_CMD_APT_INSTALL="export DEBIAN_FRONTEND=noninteractive; \
-export LC_ALL=C; \
-export LANGUAGE=C; \
-export LANG=C; \
-export DEBIAN_PRIORITY=critical; \
-export DEBCONF_NONINTERACTIVE_SEEN=true; \
-apt-get install -qy -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\""
+ROOTFS_CMD_APT_INSTALL="apt-get install -qy -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\""
+EXPORTS="export DEBIAN_FRONTEND=noninteractive
+export LC_ALL=C
+export LANGUAGE=C
+export LANG=C
+export DEBIAN_PRIORITY=critical
+export DEBCONF_NONINTERACTIVE_SEEN=true"
+DOCKERREPO='deb [arch=armhf] https://download.docker.com/linux/ubuntu bionic stable'
 
 function add_source_list() {
     echo_yellow "Adding source list"
@@ -24,19 +24,26 @@ function add_source_list() {
 
 function add_docker_repo(){
     echo_yellow "Adding new Docker repository"
-    chroot mnt/ /bin/bash -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -"
-    chroot mnt/ /bin/bash -c "echo 'deb [arch=armhf] https://download.docker.com/linux/ubuntu bionic stable' >> /etc/apt/sources.list"
-    chroot mnt/ /bin/bash -c "apt-get update -y -qq"
+    chroot mnt/ /bin/bash <<EOF
+gpg-agent
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+echo '${DOCKERREPO}' >> /etc/apt/sources.list
+apt-get update -y -qq
+EOF
 }
 
 function set_locales(){
     echo_yellow "Setup Locale"
-    chroot mnt/ /bin/bash -c "export DEBIAN_FRONTEND=noninteractive; locale-gen en_US.UTF-8 && dpkg-reconfigure --frontend noninteractive locales && update-locale LANG=en_US.UTF-8"
+    chroot mnt/ /bin/bash <<EOF
+locale-gen en_US.UTF-8
+dpkg-reconfigure --frontend noninteractive locales
+update-locale LANG=en_US.UTF-8
+EOF
 }
 
 function install_packages() {
     # Update
-    echo_yellow Installing update...
+    echo_yellow "Installing update..."
     chroot mnt/ /bin/bash -c "apt-get update -y"
     chroot mnt/ /bin/bash -c "apt-get upgrade -y"
 
@@ -44,10 +51,16 @@ function install_packages() {
     local base_packages="openssh-server ifupdown haveged policykit-1 curl iw module-init-tools ntp unzip usbutils wireless-tools wget wpasupplicant sysfsutils git i2c-tools python3-pip manpages wireless-regdb net-tools ca-certificates gnupg-agent linux-firmware vim"
 
     echo_yellow "Installing basic packages..."
-    chroot mnt/ /bin/bash -c "${ROOTFS_CMD_APT_INSTALL} $base_packages"
+    chroot mnt/ /bin/bash <<EOF
+${EXPORTS}
+${ROOTFS_CMD_APT_INSTALL} $base_packages
+EOF
     #Docker
     add_docker_repo
     echo_yellow "Installing docker packages"
     local docker_packages="docker-ce docker-ce-cli docker-compose"
-    chroot mnt/ /bin/bash -c "${ROOTFS_CMD_APT_INSTALL} $docker_packages"
+    chroot mnt/ /bin/bash <<EOF
+${EXPORTS}
+${ROOTFS_CMD_APT_INSTALL} $docker_packages
+EOF
 }
